689f2fe3b8cea5ab841d97d8b0a473242344a21d
Antonella Rocha <anto.e.rocha95@gmail.com>
2024-08-23 21:49:34 -0300

    cambios en impacto

--- src/Components/Impacto.jsx
+++ src/Components/Impacto.jsx
@@ -5,71 +5,72 @@ function Impacto() {
   return (
 
     <><section className="quienes-somos">
-      <div className="container py-5">
-        <div className="d-inline-flex mb-5">
-          <div className="bg-white mb-5 p-5 py-3 rounded-3">
-            <span className="fw-bold h1 m-5 text-black">Impacto</span>
-          </div>
-        </div>
-
-        <div className="row text-end text-white">
-          <div className="mb-5">
-            <span className="fs-1 fw-bold">ChallengeFunDemos / MSA</span>
-          </div>
-        </div>
-
-
-        <div className="fs-3 text-white mb-5 lh-s">
-          <p>
-            10 días increíbles en los que 12 chicos y chicas de las fundaciones
-            “Media Pila”, “Empujar” e “Integrar”, pudieron poner en práctica lo
-            aprendido en sus cursos de programación y conocer cómo es el trabajo
-            en equipo en un proyecto de tecnología como ámbito profesional.
-          </p>
-          <p>
-            Los participantes trabajaron en un desafío preparado por los
-            referentes de Grupo MSA y se llevaron una experiencia profesional
-            real, pasando por todo el recorrido de desarrollo de un proyecto, trabajando en equipo con los diferentes roles de los equipos de IT MSA,
-            e interactuando con todas las áreas de la compañía.
-          </p>
-          <p>
-            Además de programar los chicos y chicas que participaron, tuvieron su
-            inducción, presentaron la demo del proyecto, realizaron una retro y se
-            llevaron en el feedback de cierre valiosas recomendaciones y su perfil
-            conductual.
-          </p>
-        </div>
-
-        <div className="d-flex">
-          <div className="bg-white mx-auto px-5 py-3 rounded p-2">
-            <span className="fw-bold h2 text-black p-2">Galería de Fotos</span>
-          </div>
-        </div>
-
-
-        <div id="carouselExampleFade" className="carousel slide carousel-fade">
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img src="/img/1710776089905.jpg" class="d-block w-100" alt="img1" />
-            </div>
-            <div className="carousel-item">
-              <img src="/img/1710776101275.jpg" class="d-block w-100" alt="img2" />
-            </div>
-            <div className="carousel-item">
-              <img src="img/IMG_20240315_174910839.jpg" class="d-block w-100" alt="img3" />
+      <div className="container-fluid py-5">
+        <div className="row">
+          {/* <!-- Contenedor de "Impacto" --> */}
+          <div className="col-md-4 d-flex justify-content-center align-items-start">
+            <div className="bg-white mb-5 p-5 py-3 rounded-3">
+              <span className="fw-bold h1 m-5 text-black">Impacto</span>
             </div>
           </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+
+          {/* <!-- Contenedor de Texto Principal --> */}
+          <div className="col-md-8">
+            <div className="d-flex justify-content-around text-white ms-5 mt-5">
+              <span className="h1 fw-bold">ChallengeFunDemos / MSA</span>
+            </div>
+          </div>
+
+
+          <div className="container">
+            <div className="justify-content-center fs-2 fw-bold lh-s text-white">
+              <p className="ms-4 px-2">
+                10 días increíbles en los que 12 chicos y chicas de las fundaciones Media Pila”, Empujar e Integrar”, pudieron poner en práctica lo aprendido en sus cursos de programación y conocer cómo es el trabajo en equipo en un proyecto de tecnología como ámbito profesional.
+              </p>
+              <p className="ms-4 px-2">
+                Los participantes trabajaron en un desafío preparado por los referentes de Grupo MSA y se llevaron una experiencia profesional real, pasando por todo el recorrido de desarrollo de un proyecto, trabajando en equipo con los diferentes roles de los equipos de IT MSA, e interactuando con todas las áreas de la compañía.
+              </p>
+              <p className="ms-4 px-2">
+                Además de programar los chicos y chicas que participaron, tuvieron su inducción, presentaron la demo del proyecto, realizaron una retro y se llevaron en el feedback de cierre valiosas recomendaciones y su perfil conductual.
+              </p>
+            </div>
+          </div>
         </div>
+
+
+        <div className="container-fluid py-5">
+          <div className="position-relative">
+            <div id="carouselExampleFade" className="carousel slide carousel-fade">
+              <div className="carousel-inner rounded-3">
+                <div className="carousel-item active">
+                  <img src="/img/1710776089905.jpg" class="d-block w-100" alt="img1" />
+                </div>
+                <div className="carousel-item">
+                  <img src="/img/1710776101275.jpg" class="d-block w-100" alt="img2" />
+                </div>
+                <div className="carousel-item">
+                  <img src="img/IMG_20240315_174910839.jpg" class="d-block w-100" alt="img3" />
+                </div>
+              </div>
+              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
+
+
+            {/* Galería de Fotos superpuesta */}
+            <div className="position-absolute end-0 bg-white px-4 py-2 rounded m-3 d-flex justify-content-center align-items-center" style={{ zIndex: 1, top: '-50px' }}>
+              <span className="fw-bold h2 text-black text-nowrap pd-2">Galería de Fotos</span>
+            </div>
+          </div>
+        </div>
+
       </div>
-
 
     </section><section className="diagnostico">
         <div className="container">
