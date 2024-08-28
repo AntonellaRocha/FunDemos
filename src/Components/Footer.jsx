b7fe17d90e29effe6e3b0f5f5348023e7eb184a4
Antonella Rocha <anto.e.rocha95@gmail.com>
2024-08-27 21:07:16 -0300

    modificaciones en el footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,36 +3,41 @@ import "../App.css"
 
 const Footer = () => {
   return (
-    <footer className="d-flex justify-content-around justify-content-evenly bg-white p-3">
-      <div className="d-flex justify-content-around align-items-baseline">
-        <img
-          src="/img/LOGO FUNDEMOS.png"
-          alt="FunDemos"
-          className="ms-5 mx-4"
-          style={{ width: '150px', height: 'auto' }} // Ajustar tamaño
-        />
-        <img
-          src="/img/GRUPO MSA.png"
-          alt="Grupo MSA"
-          className="ms-5 mx-4"
-          style={{ width: '150px', height: 'auto' }} // Ajustar tamaño
-        />
-        <div className="nav align-items-center p-3 h5">
-          <a href="#quienes-somos" className="nav-link mx-0 mx-lg-1 text-dark">¿Quiénes somos?</a>
-          <a href="#areas-de-accion" className="nav-link mx-0 mx-lg-1 text-dark me-3">Áreas de Acción</a>
-          <a href="#autoridades" className="nav-link mx-0 mx-lg-1 text-dark me-3">Autoridades</a>
-          <a href="#impacto" className="nav-link mx-0 mx-lg-1 text-dark me-3">Impacto</a>
+    <footer className="bg-white p-3">
+      <div className="container d-flex flex-column flex-md-row justify-content-between align-items-center">
+        <div className="d-flex align-items-center justify-content-center justify-content-md-start mb-3 mb-md-0">
+          <img
+            src="/img/LOGO FUNDEMOS.png"
+            alt="FunDemos"
+            className="ms-5 mx-4"
+            style={{ maxWidth: '20vh', height: 'auto' }}
+          />
+          <img
+            src="/img/GRUPO MSA.png"
+            alt="Grupo MSA"
+            className="ms-5 mx-4"
+            style={{ maxWidth: '20vh', height: 'auto' }}
+          />
+        </div>
+
+        <nav className="nav flex-column flex-md-row align-items-center h5">
+          <a href="#quienes-somos" className="nav-link text-dark">¿Quiénes somos?</a>
+          <a href="#areas-de-accion" className="nav-link text-dark me-3">Áreas de Acción</a>
+          <a href="#autoridades" className="nav-link text-dark me-3">Autoridades</a>
+          <a href="#impacto" className="nav-link text-dark me-3">Impacto</a>
           <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
             <img
               src="/img/linkedin-logo.png"
               alt="LinkedIn"
-              className="ms-5 mx-4 p-2"
-              style={{ width: '70px', height: 'auto' }} // Ajustar el tamaño
+              className="mx-2"
+              style={{ maxWidth: '8vh', height: 'auto' }}
             />
           </a>
-        </div>
+        </nav>
       </div>
     </footer>
+
+
 
   );
 };
